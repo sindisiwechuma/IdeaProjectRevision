26f333b1784f9ce4e8ee86ba4524f281e376180b
Sindisiwe Chuma <sindi.chuma@gmail.comexitq!git config --global user.email sindi.chuma@gmail.com>
2021-12-21 18:51:10 +0200

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+//Package statement
+package com.example.helloworld;
+
+//Standard Java File has the same name as the class HelloWorld
+public class HelloWorld {
+    // Type main and use the existing template to avoid
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+    }
+
+
+}
